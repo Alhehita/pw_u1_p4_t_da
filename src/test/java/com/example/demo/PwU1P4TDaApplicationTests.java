e15dc9efa1a230c4f9581931ce3a2031f09511c2
Dennisse Andrade <daandradem1@uce.edu.ec>
2023-05-15 12:02:28 -0500

    Tarea 3

---
+++ src/test/java/com/example/demo/PwU1P4TDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwU1P4TDaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
